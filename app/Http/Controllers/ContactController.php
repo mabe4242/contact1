fa325af0ecd29b44128dd3c0971d2fbac0d65968
mabe4242 <kouiy42@gmail.com>
2025-07-04 01:35:46 +0900

    confirmメソッドの実装

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -9,4 +9,9 @@ class ContactController extends Controller
     public function index(){
         return view('index');
     }
+
+    public function confirm(Request $request){
+        $contact = $request->only(['name', 'email', 'tel', 'content']);
+        return view('confirm', compact('contact'));
+    }
 }
